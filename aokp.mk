44d5eac0bc0fe6f307b40768327293aeb2497746
Mehmet KÖKLÜ <selvatorxm@gmail.com>
2018-02-16 17:08:09 +0300

    AOKP Compatible

--- aokp.mk
+++ aokp.mk
@@ -1,13 +1,9 @@
 $(call inherit-product, device/lge/f400/full_f400.mk)
 
-# Inherit some common Aicp stuff.
-$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+# Inherit some common Aokp stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
-PRODUCT_NAME := aicp_f400
-
-# AICP Device Maintainers
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    DEVICE_MAINTAINERS="Mehmet KÖKLÜ (croatan)"
+PRODUCT_NAME := aokp_f400
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
